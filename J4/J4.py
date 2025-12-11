4736800160149656eaac50466fa66472b3c308f0
TanguyBod <tanguy.bodin@student.isae-supaero.fr>
2025-12-11 14:39:53 +0100

    J4 Part 2 finished

--- J4/J4.py
+++ J4/J4.py
@@ -38,4 +38,21 @@ print("Total part 1: ", total_1)
 
 # Part 2
 
+def nb_removable (array: np.ndarray, row: int, col: int) :
+    total = 0
+    modified_array = array.copy()
+    for r in range(nb_rows):
+        for c in range(nb_cols):
+            if data_array[r, c] == 1 and is_accessible(data_array, r, c):
+                total += 1
+                modified_array[r, c] = 0
+
+    return total, modified_array
+
+while True :
+    removable, data_array = nb_removable(data_array, r, c)
+    if removable == 0 :
+        break
+    total_2 += removable
+
 print("Total part 2: ", total_2)
