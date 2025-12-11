401207e2bff1d75e3ca04997cfffa125ae884cb7
tanguy.bodin <tanguy.bodin@sii.fr>
2025-12-11 14:19:37 +0100

    Day 3 Part 2 Finished

--- J3/J3.py
+++ J3/J3.py
@@ -35,4 +35,24 @@ print("Total part 1: ", total_1)
 
 # Part 2
 
+def process_bank_p2(bank : str) -> int :
+    joltage = ''
+    number_to_add = 12
+    last_index_used = 0
+    bank_list = [int(x) for x in bank.strip()]
+
+    for i in range(number_to_add, 0, -1) :
+        # Remove i - 1 last elements and start at last_index_used
+        tmp_bank_list = bank_list[last_index_used:-i+1] if i > 1 else bank_list[last_index_used:]
+        max_index = tmp_bank_list.index(max(tmp_bank_list)) + last_index_used
+        joltage += str(bank_list[max_index])
+        bank_list[max_index] = 0
+        last_index_used = max_index + 1
+    
+    return int(joltage)
+
+for line in data:
+    joltage = process_bank_p2(line)
+    total_2 += joltage
+
 print("Total part 2: ", total_2)
